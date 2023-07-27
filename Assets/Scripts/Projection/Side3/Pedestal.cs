7819a8e4e5d0485cf12279551bb3f95495a9a4dc
vongabrieltan <vongabrieltan@gmail.com>
2023-07-27 17:22:29 +0800

    Add miniBoulder and miniPond retry

--- Assets/Scripts/Projection/Side3/Pedestal.cs
+++ Assets/Scripts/Projection/Side3/Pedestal.cs
@@ -14,6 +14,10 @@ public class Pedestal : InteractableObject
     public bool isSequenceCorrect = false;
     Vector3 miniFacePosition = new Vector3(3.58f, 2.88f, -9.0f);
     Quaternion miniFaceRotation = Quaternion.Euler(-90,0,0);
+    Vector3 miniPondPosition = new Vector3(-3.413747f, 2.845497f, -8.995007f);
+    Quaternion miniPondRotation = Quaternion.Euler(0, 180, 0);
+    Vector3 miniBoulderPosition = new Vector3(0.131f, 3.107747f, -9.132f);
+    Quaternion miniBoulderRotation = Quaternion.Euler(0, 180, 0);
     // Start is called before the first frame update
     void Start()
     {
@@ -46,6 +50,22 @@ public class Pedestal : InteractableObject
         }
     }
 
+    public void InstantiateMiniItem()
+    {
+        if (correctSequence == 1)
+        {
+            Instantiate(miniItem, miniFacePosition, miniFaceRotation);
+        }
+        else if (correctSequence == 2)
+        {
+            Instantiate(miniItem, miniPondPosition, miniPondRotation);
+        }
+        else if (correctSequence == 3)
+        {
+            Instantiate(miniItem, miniBoulderPosition, miniBoulderRotation);
+        }
+    }
+
     public void CheckAttempt()
     {
         if(itemPlaced == true && otherPedestal1.itemPlaced == true && otherPedestal2.itemPlaced == true)
@@ -58,7 +78,7 @@ public class Pedestal : InteractableObject
             {
                 //spawn items above pedestal needs improvement
                 //miniItem.SetActive(true);
-                Instantiate(miniItem, miniFacePosition, miniFaceRotation);
+                InstantiateMiniItem();
                 Debug.Log("Incorrect sequence");
             }
         }
@@ -72,7 +92,7 @@ public class Pedestal : InteractableObject
             InventoryManager.Instance.Remove(puzzleItem);
             //spawn item is currently set active only. need to edit pickup to not destroy mini item
             //miniItem.SetActive(true);
-            Instantiate(miniItem, miniFacePosition, miniFaceRotation);
+            InstantiateMiniItem();
             itemPlaced = true;
             sequence = 1 + otherPedestal1.CheckItemPlaced() + otherPedestal2.CheckItemPlaced();
             CheckSequence();
